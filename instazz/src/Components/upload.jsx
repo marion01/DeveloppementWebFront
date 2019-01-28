b13a9d09461fe11e90ad4aa884b7258200ff4b39
marion <marion_bertoni@outlook.fr>
2019-01-28 19:30:18 +0100

    début preview post

--- instazz/src/Components/upload.jsx
+++ instazz/src/Components/upload.jsx
@@ -1,7 +1,75 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class Upload extends Component{
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import SendIcon from '@material-ui/icons/Send';
+
+const styles = theme => ({
+
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+        marginLeft: 'auto',
+        [theme.breakpoints.up('sm')]: {
+            marginRight: -8,
+        },
+    },
+
+    left: {
+        align: 'right'
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+
+    card: {
+        display: 'block',
+        width: '50vw',
+        transitionDuration: '0.3s',
+    },
+
+    commentaire: {
+        resize: 'vertical',
+        width: '40vw'
+    },
+});
+
+class Upload extends Component{
+    state = {
+        date: '',
+        textPost: '',
+        previewClicked: false,
+        file: ''
+    }
 
     savePost = () => {
         console.log("savePost")
@@ -63,21 +131,75 @@ export default class Upload extends Component{
         this.saveImage()
     }
 
+    miseAJourPreview = () => {
+        console.log("test")
+        this.setState({previewClicked: true})
 
-  render() {
+        let date = new Date()
+
+        this.setState({ date: date.toDateString() })
+        console.log(this.state.date)
+
+        this.setState({ textPost: document.getElementById('description').value })
+
+        let file = document.getElementById('file-input').files[0];
+        this.setState({file: file})
+    }
+
+
+    render() {
+        const { classes } = this.props;
+        var message;
+        if (this.state.previewClicked) {
+            message = <div>
+                <br></br>
+                <Card className={classes.card}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="Recipe" className={classes.avatar}>R</Avatar>
+                        }
+                        title="utilisateur connecté"
+                        subheader={this.state.date}
+                    />
+                    <CardMedia
+                        className={classes.media}
+                        image={this.state.file}
+                    />
+                    <CardContent>
+                        <Typography component="p">{this.state.textPost}</Typography>
+                    </CardContent>
+                </Card>
+                <br></br>
+                <br></br>
+                 <br></br>
+            </div>
+        } else {
+            message = <div></div>
+        }
+        
       return <div className="App-corps">
               <h2>Charger un post</h2>
               <br></br>
               <div name="form">
-                    <input id="file-input" type="file" name="photo" />
+                   <input id="file-input" type="file" name="photo" />
                   <br></br>
                   <br></br>
                   <label htmlFor="description">Entrez une description:  </label>
                   <textarea name="description" rows="4" cols="30" id="description" type="text" />
                   <br></br>
                   <br></br>
-              <input onClick={this.submitPost} type="button" value="Valider"/>
+              <input onClick={this.submitPost} type="button" value="Valider" />
+              <input onClick={this.miseAJourPreview} type="button" value="Visualisation" />
                </div>
+
+          {message}
             </div>
       }
- }
+}
+
+
+Upload.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Upload);
